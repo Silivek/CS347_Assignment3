17e44c24725612ed367739bd69759b46c0fdc681
Silivek <wkilpatrick90@gmail.com>
2023-11-13 15:53:23 -0600

    added bomb dropping and modified enemy stats
    big ships drop faster but are slower, small ships drop slower but are faster.
    bombs currently do nothing

--- Assets/Scripts/AlienBehavior.cs
+++ Assets/Scripts/AlienBehavior.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class AlienBehavior : MonoBehaviour
 {
     public float moveSpeed = 3;
+    public float bombFrequency;
+    float bombTimer;
+    public GameObject bombPrefab;
     public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        bombTimer = bombFrequency;
         rb = GetComponent<Rigidbody>();
         rb.velocity = new Vector3(-moveSpeed,0,0);
     }
@@ -22,5 +26,12 @@ public class AlienBehavior : MonoBehaviour
 
         if (transform.position.x <= -30) 
             rb.velocity = new Vector3(moveSpeed,0,0);
+
+        if (bombTimer <= 0)
+        {
+            Instantiate(bombPrefab, gameObject.transform.position, Quaternion.Euler(30, 180, 0));
+            bombTimer = bombFrequency;
+        }
+        else bombTimer -= Time.deltaTime;
     }
 }
